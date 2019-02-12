a1d65000cd145d92c21ca3123123ddaf8d2a2d75
skyuniorg <skyuni.info@gmail.com>
2019-02-11 23:47:04 -0800

    Add course, lecture to dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_012443) do
+ActiveRecord::Schema.define(version: 2019_02_12_072848) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,27 @@ ActiveRecord::Schema.define(version: 2019_02_10_012443) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "price"
+    t.string "instructor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lectures", force: :cascade do |t|
+    t.string "title"
+    t.text "note"
+    t.string "video"
+    t.boolean "header", default: false, null: false
+    t.string "tag"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lectures_on_course_id"
   end
 
   create_table "users", force: :cascade do |t|
